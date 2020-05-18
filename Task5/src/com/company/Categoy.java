c32a7886a60b5e73aa2aebc1e748536e6ca797e5
s21029 <61780228+s21029@users.noreply.github.com>
2020-05-18 15:22:24 +0200

    Update Categoy.java

--- Task5/src/com/company/Categoy.java
+++ Task5/src/com/company/Categoy.java
@@ -1,6 +1,40 @@
-package com.company;
+import java.util.ArrayList;
 
 public class Category {
 
+    ArrayList<String> Products ;
+    String Name;
 
+    public Category(){};
+    public Category(String name) {
+        this.Name = name;
+    }
+
+    public void setName(String name) {
+        this.Name = name;
+    }
+    public String getName() {
+        return Name;
+    }
+
+
+    public ArrayList<String> getProducts() {
+        return Products;
+    }
+    public void setProducts(ArrayList<String> products) {
+        Products = products;
+    }
+
+
+
+    public Category(String name, ArrayList<String> Product ) {
+        this.Name = name;
+        this.Products = Products;
+    }
+
+
+    public void setProducts(String a) {
+    }
 }
+
+
