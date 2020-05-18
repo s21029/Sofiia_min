4b83b6fefb47d20d29091d30e531bf9e3b7b6ef0
s21029 <61780228+s21029@users.noreply.github.com>
2020-05-18 15:21:12 +0200

    Add files via upload

---
+++ Task5/src/com/company/Busket.java
@@ -0,0 +1,34 @@
+import java.util.ArrayList;
+
+public class Busket {
+
+    String Name;
+    ArrayList<com.company.Product> Product;
+
+    public Busket(){}
+    public Busket(String name) {
+        Name = name;
+    }
+    public Busket(String name, ArrayList<com.company.Product> product) {
+        Name = name;
+        Product = Product;
+    }
+    public String getName() {
+        return Name;
+    }
+    public void setName(String name) {
+        Name = name;
+    }
+    public ArrayList getProducts() {
+        return Product;
+    }
+    public void setProduct(ArrayList<com.company.Product> product) {
+        Product = Product;
+    }
+
+
+
+
+
+}
+
